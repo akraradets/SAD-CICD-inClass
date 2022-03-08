81ae83384aa3224e73d4bf8b7429d6b44420d161
Akraradet Sinsamersuk <akraradets@gmail.com>
2022-03-08 16:03:41 +0700

    update allow host

--- docs/forfun/settings.py
+++ docs/forfun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=_pjg%0scnj(yomu51$pifb-1g6ujfr0k_-8g=0m_35=3^&@tx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
